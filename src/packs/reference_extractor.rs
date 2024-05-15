507e8490e1685880f6bd786316b4a367becd0e07
Perry Hertler <perry.hertler@gusto.com>
2024-05-15 16:28:42 -0500

    stash

--- src/packs/reference_extractor.rs
+++ src/packs/reference_extractor.rs
@@ -1,4 +1,7 @@
-use std::{collections::{HashMap, HashSet}, path::PathBuf};
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+};
 
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use ruby_references::configuration::ExtraReferenceFieldsFn;
@@ -9,7 +12,10 @@ use crate::packs::{
     parsing::ruby::rails_utils, process_files_with_cache, ProcessedFile,
 };
 
-use super::{checker::reference::Reference, file_utils::expand_glob, Configuration, SourceLocation};
+use super::{
+    checker::reference::Reference, file_utils::expand_glob, Configuration,
+    SourceLocation,
+};
 
 pub(crate) fn get_all_references(
     configuration: &Configuration,
@@ -105,6 +111,7 @@ impl PackageNames {
         names.sort();
         PackageNames { names }
     }
+    // TODO: build up hash of dir -> pack as we go
     pub fn find_pack_name(&self, file_path: &str) -> Option<String> {
         let mut pack_name = ".";
         let mut containing = false;
@@ -126,26 +133,32 @@ impl PackageNames {
 
 impl ExtraReferenceFieldsFn for PackageNames {
     fn extra_reference_fields_fn(
-            &self,
-            relative_referencing_file: &str,
-            relative_defining_file: Option<&str>,
-        ) -> HashMap<String, String> {
-            let mut extra_fields = HashMap::new();
-            if let Some(referencing_pack) = self.find_pack_name(relative_referencing_file) {
-                extra_fields.insert("referencing_pack_name".to_string(), referencing_pack);
-            }
-            if let Some(defining_file) = relative_defining_file {
-                if let Some(defining_pack) = self.find_pack_name(defining_file) {
-                    extra_fields.insert("defining_pack_name".to_string(), defining_pack);
-                }
-            }
+        &self,
+        relative_referencing_file: &str,
+        relative_defining_file: Option<&str>,
+    ) -> HashMap<String, String> {
+        let mut extra_fields = HashMap::new();
+        if let Some(referencing_pack) =
+            self.find_pack_name(relative_referencing_file)
+        {
             extra_fields
+                .insert("referencing_pack_name".to_string(), referencing_pack);
+        }
+        if let Some(defining_file) = relative_defining_file {
+            if let Some(defining_pack) = self.find_pack_name(defining_file) {
+                extra_fields
+                    .insert("defining_pack_name".to_string(), defining_pack);
+            }
+        }
+        extra_fields
     }
 }
 
-fn autoload_paths_from_config(configuration: &Configuration) -> HashMap<PathBuf, String> {
+fn autoload_paths_from_config(
+    configuration: &Configuration,
+) -> HashMap<PathBuf, String> {
     let mut autoload_paths: HashMap<PathBuf, String> = configuration
-    .pack_set
+        .pack_set
         .packs
         .iter()
         .flat_map(|pack| pack.default_autoload_roots())
@@ -172,20 +185,20 @@ fn autoload_paths_from_config(configuration: &Configuration) -> HashMap<PathBuf,
 
 pub(crate) fn get_all_references_new(
     configuration: &Configuration,
-    _absolute_paths: &HashSet<PathBuf>,
+    absolute_paths: &HashSet<PathBuf>,
 ) -> anyhow::Result<Vec<Reference>> {
     let pack_names = PackageNames::new(configuration);
 
     // todo: still need the special case roots
     let extra_reference_fields_fn =
-            Some(Box::new(pack_names) as Box<dyn ExtraReferenceFieldsFn>);
+        Some(Box::new(pack_names) as Box<dyn ExtraReferenceFieldsFn>);
     let ref_config = ruby_references::configuration::Configuration {
         absolute_root: configuration.absolute_root.clone(),
         autoload_paths: autoload_paths_from_config(configuration),
         acronyms: rails_utils::get_acronyms_from_disk(
             &configuration.inflections_path,
         ),
-        included_files: configuration.included_files.clone(),
+        included_files: absolute_paths.clone(),
         include_reference_is_definition: false,
         extra_reference_fields_fn,
         ..Default::default()
@@ -198,25 +211,26 @@ pub(crate) fn get_all_references_new(
             if r.extra_fields.get("referencing_pack_name").is_none() {
                 None
             } else {
-            Some(Reference {
-                constant_name: r.constant_name,
-                defining_pack_name: r
-                    .extra_fields
-                    .get("defining_pack_name")
-                    .cloned(),
-                relative_defining_file: r.relative_defining_file,
-                referencing_pack_name: r
-                    .extra_fields
-                    .get("referencing_pack_name")
-                    .cloned()
-                    .unwrap(),
-                relative_referencing_file: r.relative_referencing_file,
-                source_location: SourceLocation {
-                    line: r.source_location.line,
-                    column: r.source_location.column,
-                },
+                Some(Reference {
+                    constant_name: r.constant_name,
+                    defining_pack_name: r
+                        .extra_fields
+                        .get("defining_pack_name")
+                        .cloned(),
+                    relative_defining_file: r.relative_defining_file,
+                    referencing_pack_name: r
+                        .extra_fields
+                        .get("referencing_pack_name")
+                        .cloned()
+                        .unwrap(),
+                    relative_referencing_file: r.relative_referencing_file,
+                    source_location: SourceLocation {
+                        line: r.source_location.line,
+                        column: r.source_location.column,
+                    },
+                })
             }
-        )}})
+        })
         .collect();
 
     Ok(pks_references)
--- tests/check_test.rs
+++ tests/check_test.rs
@@ -206,8 +206,8 @@ fn test_check_contents() -> Result<(), Box<dyn Error>> {
         .assert()
         .failure()
         .stdout(predicate::str::contains("2 violation(s) detected:"))
-        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:6:4\nDependency violation: `::Bar` belongs to `packs/bar`, but `packs/foo/package.yml` does not specify a dependency on `packs/bar`."))
-        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:6:4\nPrivacy violation: `::Bar` is private to `packs/bar`, but referenced from `packs/foo`"));
+        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:3:4\nDependency violation: `::Bar` belongs to `packs/bar`, but `packs/foo/package.yml` does not specify a dependency on `packs/bar`."))
+        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:3:4\nPrivacy violation: `::Bar` is private to `packs/bar`, but referenced from `packs/foo`"));
 
     common::teardown();
     Ok(())
@@ -232,7 +232,7 @@ fn test_check_contents_ignoring_recorded_violations(
         .assert()
         .failure()
         .stdout(predicate::str::contains("1 violation(s) detected:"))
-        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:6:4\nDependency violation: `::Bar` belongs to `packs/bar`, but `packs/foo/package.yml` does not specify a dependency on `packs/bar`."));
+        .stdout(predicate::str::contains("packs/foo/app/services/foo.rb:3:4\nDependency violation: `::Bar` belongs to `packs/bar`, but `packs/foo/package.yml` does not specify a dependency on `packs/bar`."));
 
     common::teardown();
     Ok(())
